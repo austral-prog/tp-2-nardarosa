4ea0b714147fd935798c6f5c878f4edcd1a66277
nardarosa <nardarosa2050@gmail.com>
2025-04-07 10:36:00 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,21 @@
 def change():
     expense = 23.75
     money = 100
+    vuelto = str(money - expense)
+    position = vuelto.find(".")
+    pesos = vuelto[:position]
+    centavos = vuelto[position + 1:]
+    print(f"Ingresar gasto:")
+    print(f"{expense}")
+    print(" ")
+    print(f"Dinero resibido:")
+    print(f"{money}")
+    print(" ")
+    print(f"Vuelto:")
+    print(" ")
+    print(f"Pesos:")
+    print(f"{pesos}")
+    print(" ")
+    print(f"Centavos:")
+    print(f"{centavos}")
+change()
